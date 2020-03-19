5f5b50d32f1ca1fdb638ea70bd8c26d946a4aabf
Yiyang Xu <xuyiyangwing@gmail.com>
2020-03-19 17:43:23 +0800

    [chore] 设置result status code类型

--- server/config.ts
+++ server/config.ts
@@ -1,22 +1,25 @@
-export default {
-    mysql: {
-        host: 'rm-bp1r8386h2f0n9713jo.mysql.rds.aliyuncs.com',
-        user: 'zou',
-        password: 'Zouzou123',
-        database: 'achieveit',
-        port: '3306'
-    },
-    jwt: {
-        signKey: "achieveit_project_sign_key"
-    },
-    status: {
-        NOT_FOUND: 'not found',
-        SUCCESS: 'ok',
-        ERROR: 'error'
-    },
-    msg: {
-        GET_MEMBER: "success",
-        UPDATE_MEMBER: "success"
-    }
-}
+import { StatusType } from "achieve-it-contract";
 
+const config: { [key: string]: any; status: { [key: string]: StatusType } } = {
+  mysql: {
+    host: "rm-bp1r8386h2f0n9713jo.mysql.rds.aliyuncs.com",
+    user: "zou",
+    password: "Zouzou123",
+    database: "achieveit",
+    port: "3306"
+  },
+  jwt: {
+    signKey: "achieveit_project_sign_key"
+  },
+  status: {
+    NOT_FOUND: "not found",
+    SUCCESS: "ok",
+    ERROR: "error"
+  },
+  msg: {
+    GET_MEMBER: "success",
+    UPDATE_MEMBER: "success"
+  }
+};
+
+export default config;
