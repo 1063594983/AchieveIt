5dc248855e241b60256c41640a0fce0810a88172
Qin Zheyu <eternalqzy@gmail.com>
2020-04-08 15:38:11 +0800

    风险管理页面更新

--- client/src/views/projects/risk.vue
+++ client/src/views/projects/risk.vue
@@ -1,22 +1,26 @@
 <template>
   <div>
-    <h1>风险管理</h1>
-    <pre>{{ riskDetail }}</pre>
+    <h1>风险管理 </h1>
+    <el-table :data="riskList">
+      <el-table-column label="ID" prop="risk_id" width="100px"></el-table-column>
+      <el-table-column label="项目名称" prop="project_id"></el-table-column>
+      <el-table-column label="内容" prop="detail"></el-table-column>
+      <el-table-column label="处理状态" prop="solve_status"></el-table-column>
+    </el-table>
   </div>
 </template>
-
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { userStore } from '@/store';
 import agent from '@/agent';
+import { Risk } from 'achieve-it-contract';
 
 @Component
-export default class Risk extends Vue {
-  riskDetail = 'loading...';
+export default class Risks extends Vue {
+  riskList: Risk[] = [];
   async mounted() {
-    const user = userStore.currentUser!;
-    const result = await agent.risk.get('4', { token: user.token });
-    this.riskDetail = JSON.stringify(result, null, 2);
+    const list = await agent.risk.ofProject(123);
+    this.riskList = list.risk_list;
+
   }
 }
 </script>
