c4a044bad919f0964eb84d37fd6c5bb62fc4db13
Qin Zheyu <eternalqzy@gmail.com>
2020-04-12 14:54:54 +0800

    风险管理页面更新

--- client/src/views/projects/risk.vue
+++ client/src/views/projects/risk.vue
@@ -2,6 +2,9 @@
   <div>
     <div class="flex items-center justify-between">
       <h1>风险管理</h1>
+      <div>
+        <el-button icon="el-icon-risk-add" @click="openCreateDialog">新建风险记录</el-button>
+      </div>
       <el-select v-model="selectedProjectId" filterable>
         <el-option
           v-for="i of projects"
@@ -17,26 +20,58 @@
       <el-table-column label="内容" prop="detail"></el-table-column>
       <el-table-column label="处理状态" prop="solve_status"></el-table-column>
     </el-table>
+    <risk-create-dialog
+            :visible="this.createDialogVisible"
+            :on-close="closeCreateDialog"
+            :on-create="insertRisk" >
+      </risk-create-dialog>
   </div>
 </template>
 <script lang="ts">
   import { Component, Vue, Watch } from "vue-property-decorator";
   import agent from "@/agent";
-  import { Project, Risk } from "achieve-it-contract";
+  import { Project, Risk, RiskPostBody } from "achieve-it-contract";
+  import RiskCreateDialog from "@/components/RiskCreateDialog.vue";
+  import { Notify } from "@/theme";
 
-  @Component
+
+  @Component({
+    components: {  RiskCreateDialog },
+  })
 export default class Risks extends Vue {
   riskList: Risk[] = [];
   projects: Project[] = [];
   selectedProjectId = null;
   isLoading = false;
+  createDialogVisible = false;
+
+    openCreateDialog() {
+      this.createDialogVisible = true;
+    }
+    closeCreateDialog() {
+      this.createDialogVisible = false;
+    }
+
+    async insertRisk(form: RiskPostBody) {
+      try {
+        if (form.detail !== null) await agent.risk.insert(form);
+        else {
+          Notify.error('细节不为空');
+          return false;
+        }
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
 
   refresh() {
     this.isLoading = true;
     agent.risk.ofProject(this.selectedProjectId).then(result => {
-      this.riskList = result.risk_list;
+        this.riskList = result.risk_list;
       this.isLoading = false;
     })
+
   }
 
   @Watch('selectedProjectId')
